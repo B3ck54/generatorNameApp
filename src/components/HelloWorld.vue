f8a362fcf4bbd7cc0d007ddb5fea280f900acdba
b3ck54 <b3ck54@gmail.com>
2020-07-04 19:07:38 +0200

    method generate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row
+    align="center">
+      <v-col
+      cols="12"
+      class="text-center">
+      <h1>Générateur de nom aléatoire</h1>
+      </v-col>
+    </v-row>
+  <v-row>
+    <v-col
+    cols="6"
+    class="text-center">
+    <v-card> {{ name }}</v-card>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col
+    cols="6"
+    class="text-center">        
+    <v-btn 
+    @click="generatorName">
+    Générer
+    </v-btn>
+    </v-col>
+  </v-row>
+  <v-row>
+  <v-col
+  cols="6"
+  class="text-center">        
+  <v-btn>
+  Reset
+  </v-btn>
+  </v-col>
+  </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+        name: '',
+        arrayAlphabet : ['AI','BA','CI','DU','EN','FA','GO','AH','PI','JO','KO','LE','MI','NY','RO','PA','QUI','RA','S','T','U','V','W','X','Y','Z'],
+    }),
+      methods: {
+      generatorName: function () {
+
+        // this.arrayConsonant.forEach(function(item, index) {
+        // alert(item, index);
+        // });
+
+        while (this.name.length < 6) {
+        this.name += this.arrayAlphabet[Math.floor(Math.random() * this.arrayAlphabet.length)];
+        return this.name
+        } 
+
+        // var pair1 = 2 * Math.floor(Math.random() * (this.pairs.length / 2));
+        // this.name += this.pairs.substr(pair1, 1);
+        // // this.name = this.name.replace(/[.]/g, "");
+        // return this.name     
+      }
+    }
+  }
+</script>
